6bc13b7f103c5bbbdf660d6a81a089217bfe1ef1
khadiza21 <bibikhadiza474@gmail.com>
2024-08-14 13:33:27 +0600

    ternary

---
+++ modernJS_lws/ternary.js
@@ -0,0 +1,9 @@
+var age = 19;
+var type = age >= 18 ? "adult" : age < 10 ? "child " : " young";
+console.log (type);
+
+
+var fruits = "apple";
+var checkFruit = fruits === 'apple' // ? true : false; // if here have true and false then don't need to write tru and false 
+
+console.log(checkFruit);
