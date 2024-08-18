4181dafe88887d029a64d83a69ba8c58e75b087c
khadiza21 <bibikhadiza474@gmail.com>
2024-08-18 22:53:33 +0600

    ternary

--- modernJS_lws/ternary.js
+++ modernJS_lws/ternary.js
@@ -7,3 +7,7 @@ var fruits = "apple";
 var checkFruit = fruits === 'apple' // ? true : false; // if here have true and false then don't need to write tru and false 
 
 console.log(checkFruit);
+let isLeader = true;
+let price = 5000; 
+price = isLeader === true ?  price > 1000 ? price/2 : 0 :  price+1000;
+console.log(price)
