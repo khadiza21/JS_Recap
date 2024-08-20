90c3dc0fba2ec7542b4327d933141e4da056e073
khadiza21 <bibikhadiza474@gmail.com>
2024-08-20 12:44:33 +0600

    split concat join

---
+++ modernJS_lws/splitConcatJoin.js
@@ -0,0 +1,21 @@
+//
+const sentence = "This is very heart broken subject. ";
+console.log(sentence.split("a"));
+console.log(sentence.split(" "));
+
+const friendStr = "Tani, Rani, Sani, Dali, Bali";
+console.log(friendStr.split(","));
+
+// [ 'This is very he', 'rt broken subject. ' ]
+// [ 'This', 'is', 'very', 'heart', 'broken', 'subject.', '' ]
+// [ 'Tani', ' Rani', ' Sani', ' Dali', ' Bali' ]
+
+// join
+const flower = ["lily", "water lily", "cosmos", "daisy", "sunflower", "rose"];
+console.log(flower.join(" --> "));
+//lily --> water lily --> cosmos --> daisy --> sunflower --> rose
+
+// concat
+const line1 = "Twinkle Twinkle";
+const line2 = "Little Star";
+console.log(line1.concat(line2));
