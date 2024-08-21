75fc2e5c72b6350c7b66d9837698dc0d9ab25efe
khadiza21 <bibikhadiza474@gmail.com>
2024-08-21 15:52:18 +0600

    Math object in JS

---
+++ modernJS_lws/Math.js
@@ -0,0 +1,35 @@
+const min = Math.min (3,43,3,66,2,75,753,56);
+const max = Math.max (3,43,3,66,2,75,753,56);
+
+console.log(min);// 2
+console.log(max); //753
+
+console.log(Math.PI);// 3.141592653589793
+console.log(Math.abs(5-10));// 5
+
+console.log(Math.round(2.25));// 2
+console.log(Math.round(2.78));// 3
+
+console.log(Math.floor(2.89));// 2
+console.log(Math.floor(3.33));// 3
+
+console.log(Math.ceil(2.002));// 3
+console.log(Math.ceil(2.999));// 3
+
+
+console.log(Math.random());// 0.8844902760540923
+console.log(Math.random()*10);// 4.497847645241897
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);// 7
+
+
+
+
+
+
+
+
+
+
+
