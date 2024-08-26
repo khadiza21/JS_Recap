be8160a3be9f4ea61356314b3cd740c17b58c39c
khadiza21 <bibikhadiza474@gmail.com>
2024-08-26 15:32:42 +0600

    validation

---
+++ JSdom/JS ES6/validation.js
@@ -0,0 +1,25 @@
+const numbers = [3,12,53,11,37];
+const student = {
+    name: 'Abdus Salam.', degree: 'BSc', department: 'CSE'
+}
+console.log(Array.isArray(numbers));// true
+console.log(typeof student);// object
+
+// isNaN
+console.log(isNaN('2'));// false
+console.log(isNaN('ab'));// true
+console.log(isNaN(56));// false
+console.log(isNaN({}));// true
+console.log(isNaN([]));// false
+console.log(isNaN([5]));// false
+
+
+// JS wired Things - WAT JS
+
+
+
+
+
+
+
+
