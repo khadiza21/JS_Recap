caed740cd860690e21d90bb9c798afae2dc99a19
khadiza21 <bibikhadiza474@gmail.com>
2024-08-27 14:56:25 +0600

    object tricks

--- modernJS_lws/objectTricks.js
+++ modernJS_lws/objectTricks.js
@@ -9,6 +9,26 @@ const keys = Object.keys(myObj);
 const values = Object.values(myObj);
 const entries = Object.entries(myObj);
 
+// delete object entries/property
+delete myObj.estd;
+console.log(myObj);
+
+// delete entries/property
+const {ranking , ...shortObje} = myObj; //here deleted the ranking property from myObj Object. and use rest operator for getting rest object property.
+
+// object Freeze (any update , add , delete not occurs by using freeze)
+Object.freeze(myObj);
+myObj.price =  343434;
+delete myObj.name;
+
+//  object seal (only update allow, add/delete will not allow )
+Object.seal(myObj);
+myObj.price = 23323;
+myObj.founder = 'C A';
+
+
+
+
 
 console.log(keys , '\n' , values , '\n' , entries) ;
 
