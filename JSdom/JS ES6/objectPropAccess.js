52c960b97879a6c3ad4e9c82375b1e8cafb72307
khadiza21 <bibikhadiza474@gmail.com>
2024-08-27 23:55:59 +0600

    object property access ways

---
+++ JSdom/JS ES6/objectPropAccess.js
@@ -0,0 +1,8 @@
+const person ={
+    name: 'BK', job:'Developer', 2:'Second'
+}
+const job = 'job'
+console.log(person.name);
+console.log(person['name'])
+console.log(person[job]);
+console.log(person['2']) // person.2 will give error. 
